d95c89c38da50b3663cb89a2a7cd105c82f32d3c
Shriram761 <shriramvenkatesh76@gmail.com>
2025-09-10 21:41:12 +0530

    Setup App for Customers

---
+++ Customer/Frontend-BAP/app/_layout.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import { Stack } from 'expo-router';
+import React from 'react';
+
+const _layout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='index' options={{ title: 'Home' }} />
+    </Stack>
+  );
+};
+
+export default _layout;
