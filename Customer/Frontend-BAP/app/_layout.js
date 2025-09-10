1f2da883d189c3a5d468943bec322e2857e8c3b7
Shriram761 <shriramvenkatesh76@gmail.com>
2025-09-10 22:03:47 +0530

    Setup NativeWind,Changed Routing Structure

--- Customer/Frontend-BAP/app/_layout.js
+++ Customer/Frontend-BAP/app/_layout.js
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native';
 import { Stack } from 'expo-router';
 import React from 'react';
+import '../global.css';
 
 const _layout = () => {
   return (
--- Customer/Frontend-BAP/app/index.js
+++ Customer/Frontend-BAP/app/index.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
 
 const index = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>index</Text>
+    <View className='items-center justify-center flex-1'>
+      <Text className='text-2xl font-bold text-blue-400'>index</Text>
+      <TouchableOpacity onPress={() => router.push('/Login')} className='my-4'>
+        <Text className='text-lg font-semibold'>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => router.push('/SignUp')} className='my-4'>
+        <Text className='text-lg font-semibold'>Signup</Text>
+      </TouchableOpacity>
     </View>
   );
 };
